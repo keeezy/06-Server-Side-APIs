1f565ec7a57492e76c8e369d17b676f79c8582f1
keeezy <lazarokev@gmail.com>
2022-08-30 16:11:08 -0700

    set the variables, got api key

---
+++ assets/js/logic.js
@@ -0,0 +1,10 @@
+// Variables
+var citySearchInputEl = document.querySelector('#city-search-input');
+var citySearchInput;
+var citySearchHistory = [];
+var citySearchHistoryBtnEl;
+var citySearchHistoryBtnContainerEl = document.querySelector('#btnList')
+var apiKey = 'a46fae6c97cce84840e8dfd333cdaca5';
+var latLonApiUrl;
+var tempK;
+var tempF;
