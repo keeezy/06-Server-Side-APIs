0efc55c1763b925772b1d2bbddd9efd80ea14cca
keeezy <lazarokev@gmail.com>
2022-08-30 16:12:14 -0700

    set variables for current day, dom manipulation for current day

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -8,3 +8,15 @@ var apiKey = 'a46fae6c97cce84840e8dfd333cdaca5';
 var latLonApiUrl;
 var tempK;
 var tempF;
+
+// Current Day
+var todayCardHeaderEl = document.querySelector('#todayCardHeader');
+var todayIconEl = document.querySelector('#todayIcon');
+var currentTempEl = document.querySelector('#currentTemp');
+var currentHumidityEl = document.querySelector('#currentHumidity');
+var currentWindSpeedEl = document.querySelector('#currentWindSpeed');
+var currentUVIndexEl = document.querySelector('#currentUV');
+var currentUVIndex;
+var weatherTodayIcon;
+var weatherTodayIconSource;
+var weatherTodayDescription;
